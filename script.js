58f184f1c55f0f89338a9c0e1f51daf640a5d8ce
Ale Candia <alejandrogcandia@gmail.com>
2025-11-20 14:58:59 -0300

    se cambia section contacto

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("contacto-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const form = document.getElementById("contacto-form");
+  const gracias = document.getElementById("mensaje-gracias");
+  const descripcion = document.getElementById("contacto-desc"); // <--- nuevo
+
+  // Ocultamos el texto también
+  descripcion.style.display = "none";
+
+  // Animación del formulario
+  form.classList.add("hidden");
+
+  setTimeout(() => {
+    form.style.display = "none";
+    gracias.classList.add("visible");
+  }, 400);
+});
